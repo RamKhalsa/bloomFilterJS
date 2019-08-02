f361243ce6d0f3fdbe97374ec101d81a9ec749e7
RamKhalsa <khalsa.ramdas@gmail.com>
2019-08-02 09:39:55 -0700

    Implement shared storage for hashing functions

--- bloomFilter.js
+++ bloomFilter.js
@@ -1,16 +1,15 @@
-const murmurhash2_32_gc = require('./hashes/murmur');
+const { murmurHash } = require('./hashes/murmur');
 const simpleHash = require('./hashes/simple');
 const fnv32a = require('./hashes/fnv');
 
-const BloomFilter = function(size, numHashes){
+const BloomFilter = function(size, numHashes) {
     this._storage = new Array(size).fill(0);
     this.size = size;
     this.numHashes = numHashes;
-    this._hashes = [murmurhash2_32_gc, simpleHash, fnv32a];
     this.numAdded = 0;
 }
 
-const hash = function(string, max, hasher, seed){
+const hash = function(string, max, hasher, seed) {
     const num = hasher(string, max, seed);
     return num > max ? num % max : num;
 }
@@ -23,7 +22,8 @@ BloomFilter.prototype = {
             this._storage[indices[i]] = 1;
         }
     },
-    mightContain:function(string){
+    _hashes: [murmurHash, simpleHash, fnv32a],
+    mightContain: function(string){
         const indices = this.getHashes(string);
         for (let i = 0; i < indices.length; i++) {
             if (this._storage[indices[i]] === 0) {
@@ -34,13 +34,15 @@ BloomFilter.prototype = {
     },
     getHashes:function(string) {
         var indices = [];
-        for (var i = 0; i < this.numHashes; i++){
+        for (var i = 0; i < this.numHashes; i++) {
             indices.push(hash(string, this.size, this._hashes[i], i));
         }
         return indices;
     },
     getFalsePositiveRate:function() {
         var eExp = ((-1) * this.numHashes * this.numAdded) / this.size;
-        return `The False Positive rate is ${Math.pow((1 - Math.pow(Math. E , (eExp))), this.numHashes).toFixed(4)} percent`;
+        return Math.pow((1 - Math.pow(Math.E, (eExp))), this.numHashes).toFixed(2);
     }
 }
+
+module.exports = BloomFilter;
--- hashes/simple.js
+++ hashes/simple.js
@@ -1,14 +1,11 @@
-function simpleHash(str, max) {
+var getIndexBelowMaxForKey = function(str, max) {
   var hash = 0;
-  if (str.length == 0) {
-      return hash;
-  }
   for (var i = 0; i < str.length; i++) {
-      var char = str.charCodeAt(i);
-      hash = ((hash << 5) - hash) + char;
-      hash = hash & hash;
+    hash = (hash << 5) + hash + str.charCodeAt(i);
+    hash = hash & hash; // Convert to 32bit integer
+    hash = Math.abs(hash);
   }
-  return Math.abs(hash) % max;
-}
+  return hash % max;
+};
 
-module.exports = simpleHash;
+module.exports = getIndexBelowMaxForKey;
